c308bf0348cfad05a006f933d6876203889aa783
VoroninNick <p.korenev@voroninstudio.eu>
2015-10-06 11:41:40 +0300

    banners migration

--- db/migrate/20150912084024_create_banners.rb
+++ db/migrate/20150912084024_create_banners.rb
@@ -1,6 +1,7 @@
 class CreateBanners < ActiveRecord::Migration
   def change
     create_table :banners do |t|
+      t.belongs_to :page
       t.string :title
       t.string :description
       t.has_attached_file :background_image
