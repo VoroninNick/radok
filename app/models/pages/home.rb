dcc54b5032abc0e0a2b48a6a28899d919f6a6984
VoroninNick <p.korenev@voroninstudio.eu>
2015-10-26 13:41:15 +0200

    home

--- app/models/pages/home.rb
+++ app/models/pages/home.rb
@@ -1,6 +1,6 @@
 class Pages::Home < Page
 
-  has_html_block :banner, :how_it_works, :what_for_you, :statistics, :plans, :devices, :feedbacks, :bottom_block
+  has_html_block :banner_1_title_with_description, :banner_2_title_with_description, :how_it_works, :what_for_you, :statistics, :plans, :devices, :feedbacks, :bottom_block
 
   def self.default_url
     "/"
