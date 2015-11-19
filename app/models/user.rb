efadd8bd4f992c95b3cc997130db1fd0df3e1b77
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-19 17:27:32 +0200

    disable validation for user.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,12 +30,13 @@ class User < ActiveRecord::Base
 
   #attr_accessor :login
 
-  validates :username, :email, presence: true
+  #validates :username, :email, presence: true
   validates :email, :username, uniqueness: true
 
   def self.from_omniauth(auth)
     auth.try do |auth|
       where("(provider = ? and uid = ?) or email = ?", auth[:provider], auth[:uid], auth[:info][:email]).first_or_create do |user|
+
         user.email = auth[:info][:email]
         user.password = Devise.friendly_token[0,20]
         user.first_name = auth[:info][:first_name]   # assuming the user model has a name
