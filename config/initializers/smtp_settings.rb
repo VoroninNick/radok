cbef983650f8d0f57c6a707e84b97d85c623080c
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-19 12:51:35 +0200

    facebook

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,19 +1,4 @@
-class Settings
-    def self.[](arg)
-        config = YAML.load_file(File.open(Rails.root.join("config/settings/development.yml")))
-        path_fragments = arg.split(".")
-        res = config
-
-        path_fragments.each do |f|
-            res = res[f]
-            if res.nil?
-                return nil
-            end
-        end
-
-        return res
-    end
-end
+require_relative 'settings'
 
 smtp_profile = :yandex
 
