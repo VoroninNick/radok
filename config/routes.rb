9cfb0b43af63cf66f2d6d0c25d84bc7d75c3b538
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-19 15:18:08 +0200

    add session_inspect to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "session", to: "application#session_inspect"
   get "w", to: "wizard#new_and_allow"
   scope "emails", controller: "emails" do
     root action: :index, as: :emails
