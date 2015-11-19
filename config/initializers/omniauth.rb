d0206614fcb519fd04bb219b494106fbf376b255
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-19 17:05:16 +0200

    add user:email to github omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Settings['omniauth.facebook.app_id'], Settings['omniauth.facebook.app_secret'], scope: "email,public_profile", info_fields: 'email'
 
   #provider :github, '7c153b36621a11e7671a', '6df628a3fb4e63869372972a0b5f94aba2e11fe6'#, scope: "user:email,user:follow"
-  provider :github, Settings['omniauth.github.client_id'], Settings['omniauth.github.client_secret']
+  provider :github, Settings['omniauth.github.client_id'], Settings['omniauth.github.client_secret'], scope: "user:email"
 
   #provider :google_oauth2, "514836369419-inlr5a4im5bir2u2cl38sqsbdujtkqe4.apps.googleusercontent.com", "Dau-5iDpCwSGutY_C7R0INx5"
   provider :google_oauth2, Settings['omniauth.google_oauth2.client_id'], Settings['omniauth.google_oauth2.client_secret']
