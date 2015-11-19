06381c75ec7f99692d78801a328a909df04808ca
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-19 16:47:56 +0200

    add github to social

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -9,7 +9,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :facebook, "574720446011703", "1a56643ae3aa26eb1008dae30118b620", scope: "email,public_profile"
   provider :facebook, Settings['omniauth.facebook.app_id'], Settings['omniauth.facebook.app_secret'], scope: "email,public_profile", info_fields: 'email'
 
-  provider :github, '7c153b36621a11e7671a', '6df628a3fb4e63869372972a0b5f94aba2e11fe6'#, scope: "user:email,user:follow"
+  #provider :github, '7c153b36621a11e7671a', '6df628a3fb4e63869372972a0b5f94aba2e11fe6'#, scope: "user:email,user:follow"
+  provider :github, Settings['omniauth.github.client_id'], Settings['omniauth.github.client_secret']
 
   #provider :google_oauth2, "514836369419-inlr5a4im5bir2u2cl38sqsbdujtkqe4.apps.googleusercontent.com", "Dau-5iDpCwSGutY_C7R0INx5"
   provider :google_oauth2, Settings['omniauth.google_oauth2.client_id'], Settings['omniauth.google_oauth2.client_secret']
