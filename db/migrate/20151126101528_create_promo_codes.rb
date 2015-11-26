cf39e8b70e2582701b920dbcf749c4520df10ab6
VoroninNick <p.korenev@voroninstudio.eu>
2015-11-26 20:39:52 +0200

    promocode unique

--- db/migrate/20151126101528_create_promo_codes.rb
+++ db/migrate/20151126101528_create_promo_codes.rb
@@ -1,10 +1,12 @@
 class CreatePromoCodes < ActiveRecord::Migration
   def change
     create_table :wizard_promo_codes do |t|
-      t.string :password
+      t.string :password, null: false
       t.float :percentage_discount
 
       t.timestamps null: false
     end
+
+    add_index :wizard_promo_codes, :password, unique: true
   end
 end
