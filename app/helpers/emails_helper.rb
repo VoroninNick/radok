cf98278e8fc716f5ad75a2b88908b222fb08bcf5
VoroninNick <p.korenev@voroninstudio.eu>
2016-01-12 11:46:35 +0200

    helper

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -7,6 +7,10 @@ module EmailsHelper
     "+1-514-585-8198"
   end
 
+  def formatted_phone_number
+    "+1-888-535-4852"
+  end
+
   def site_url
     "http://10g-force.com"
   end
@@ -14,4 +18,6 @@ module EmailsHelper
   def img_url(relative_url)
     "#{site_url}/assets/#{relative_url}"
   end
+
+
 end
