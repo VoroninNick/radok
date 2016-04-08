d162f1fa7200eb4ac4fdb62f55ec8207e266560b
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2016-04-08 09:52:39 +0300

    S3 configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
     storage: :s3,
     url: ':s3_domain_url',
     path: '/:class/:attachment/:id_partition/:style/:filename',
+    bucket: ENV['s3_bucket_name'],
     s3_credentials: {
-      bucket: ENV['s3_bucket_name'],
       access_key_id: ENV['aws_access_key_id'],
       secret_access_key: ENV['aws_secret_access_key'],
     }
