30fd24a69ed90be36a22cf466d56f384112c989b
irenekovalchuk <irene.kovaltchuk@gmail.com>
2016-05-11 13:59:40 +0300

    added google auth attributes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require_relative 'settings'
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['omniauth.facebook.app_id'], ENV['omniauth.facebook.app_secret'], scope: "email,public_profile", info_fields: 'email'
   provider :github, ENV['omniauth.github.client_id'], ENV['omniauth.github.client_secret'], scope: "user:email"
-  provider :google_oauth2, ENV['omniauth.google_oauth2.client_id'], ENV['omniauth.google_oauth2.client_secret']
+  provider :google_oauth2, ENV['omniauth.google_oauth2.client_id'], ENV['omniauth.google_oauth2.client_secret'], prompt: 'select_account'
   provider :linkedin, ENV['omniauth.linkedin.client_id'], ENV['omniauth.linkedin.client_secret']
   provider :twitter, ENV['omniauth.twitter.consumer_key'], ENV['omniauth.twitter.consumer_secret']
 end
